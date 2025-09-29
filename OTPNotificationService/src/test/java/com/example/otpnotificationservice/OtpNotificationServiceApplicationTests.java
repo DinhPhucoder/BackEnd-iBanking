2688c995ed47e5705f74dfbf0ed6c3be8827174a
QuyNg2401 <Nguyenduyquy2401@gmail.com>
2025-09-29 14:48:54 +0700

    feat: add TuitionService and OTPNotificationService

---
+++ OTPNotificationService/src/test/java/com/example/otpnotificationservice/OtpNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otpnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
